572ee8c9c222ed6800bf410d08a4188491098303
Agnieszka Szkurłat <szkurlatagnieszka@gmail.com>
2025-12-07 21:35:24 +0100

    test: add demo test for homepage title and header

---
+++ tests/demo/mytest.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test("Should load homepage with correct title", async ({ page }) => {
+    await page.goto('https://katalon-demo-cura.herokuapp.com/');
+
+    // Assert 
+    await expect(page).toHaveTitle('CURA Healthcare Service');
+    await expect(page.locator('h1')).toHaveText('CURA Healthcare Service');
+});
