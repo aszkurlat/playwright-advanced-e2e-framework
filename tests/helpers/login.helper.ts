eae2a78b41263d63a6c9e5e83035b846bf22ce2b
Agnieszka Szkurłat <szkurlatagnieszka@gmail.com>
2025-12-12 20:34:34 +0100

    Revert "test: add login flow with beforeEach setup"
    This reverts commit 84b878e26259d2bbb69da4c0642f31badf8825c3.

--- tests/helpers/login.helper.ts
+++
@@ -1,9 +0,0 @@
-import { Page } from "@playwright/test";
-
-export async function login(page: Page) {
-    await page.goto("https://katalon-demo-cura.herokuapp.com/");
-    await page.getByRole("link", { name: "Make Appointment" }).click();
-    await page.getByLabel("Username").fill("John Doe");
-    await page.getByLabel("Password").fill("ThisIsNotAPassword");
-    await page.getByRole("button", { name: "Login" }).click();
-}
