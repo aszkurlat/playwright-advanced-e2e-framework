922dbf87177419ae1a32c694214eb0895f91070e
Agnieszka Szkurłat <szkurlatagnieszka@gmail.com>
2025-12-30 21:21:58 +0100

    chore: enable dotenv in Playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,15 @@
+import { de, en } from '@faker-js/faker';
 import { defineConfig, devices } from '@playwright/test';
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({
+  path: path.resolve(process.cwd(), ".env"),
+});
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -22,8 +25,22 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+  globalSetup: require.resolve('./tests/helpers/global.setup'),
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html',
+      { open: "never" }
+    ],
+    ['allure-playwright', {
+      details: true,
+      suiteTitle: true,
+      environmentInfo: {
+        name: 'TEST ENVIRONMENT',
+        appName: 'CURA Healthcare',
+        release: '1.0.0',
+        node_version: process.version,
+      }
+    }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -31,7 +48,12 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    // this configuration helps resolve SSL certificate errors in WebKit
     ignoreHTTPSErrors: true,
+    navigationTimeout: 30_000,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    //actionTimeout: 10_000,
   },
 
   /* Configure projects for major browsers */
