6449d50c851f2b78506e6086ca069de4dfd8faf3
Agnieszka Szkurłat <szkurlatagnieszka@gmail.com>
2026-01-11 14:16:32 +0100

    feat(api): add GET and POST users tests for dummyjson API

--- tests/api/users.api.spec.ts
+++ tests/api/users.api.spec.ts
@@ -4,40 +4,40 @@ import constants from "../../data/constants.json";
 import TestData from "../../data/test-data";
 
 test.describe("Users API", () => {
-    const baseURL = process.env.API_URL ?? "https://dummyjson.com";
+  const baseURL = process.env.API_URL ?? "https://dummyjson.com";
 
-    test.describe("GET /users", () => {
-        test("should return non-empty users list", async ({ request }) => {
-            await logWithStep("info", `Making a GET call using ${baseURL}`);
+  test.describe("GET /users", () => {
+    test("should return non-empty users list", async ({ request }) => {
+      await logWithStep("info", `Making a GET call using ${baseURL}`);
 
-            const res = await request.get(`${baseURL}${constants.REQ_RES_ENDPOINTS.GET_USERS_LIST}`);
+      const res = await request.get(`${baseURL}${constants.REQ_RES_ENDPOINTS.GET_USERS_LIST}`);
 
-            expect(res.status()).toBe(200);
-            expect(res.headers()["content-type"] ?? "").toContain("application/json");
+      expect(res.status()).toBe(200);
+      expect(res.headers()["content-type"] ?? "").toContain("application/json");
 
-            const body = await res.json();
-            expect(body.users.length).toBeGreaterThan(0);
+      const body = await res.json();
+      expect(body.users.length).toBeGreaterThan(0);
 
-            await logWithStep("info", `Number of users received: ${body.users.length}`);
-        });
+      await logWithStep("info", `Number of users received: ${body.users.length}`);
     });
+  });
 
-    test.describe("POST /users/add", () => {
-        const users = TestData.apiUserCreation();
+  test.describe("POST /users/add", () => {
+    const users = TestData.apiUserDummyjsonCreation();
 
-        users.forEach((user) => {
-            test(`should create user: ${user.data.firstName} ${user.data.lastName}`, async ({ request }) => {
-                const res = await request.post(
-                    `${baseURL}${constants.REQ_RES_ENDPOINTS.POST_USER}`,
-                    { data: user.data, headers: user.headers }
-                );
+    users.forEach((user) => {
+      test(`should create user: ${user.data.firstName} ${user.data.lastName}`, async ({ request }) => {
+        const res = await request.post(
+          `${baseURL}${constants.REQ_RES_ENDPOINTS.POST_USER}`,
+          { data: user.data, headers: user.headers }
+        );
 
-                expect(res.status()).toBe(201);
-                expect(res.headers()["content-type"] ?? "").toContain("application/json");
+        expect(res.status()).toBe(201);
+        expect(res.headers()["content-type"] ?? "").toContain("azplication/json");
 
-                const body = await res.json();
-                expect(body.firstName).toBe(user.data.firstName);
-            });
-        });
+        const body = await res.json();
+        expect(body.firstName).toBe(user.data.firstName);
+      });
     });
+  });
 });
