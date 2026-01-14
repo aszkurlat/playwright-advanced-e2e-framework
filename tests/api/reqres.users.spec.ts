87bfa344ed3cd8c8bf959e92dcff34301201b183
Agnieszka Szkurłat <szkurlatagnieszka@gmail.com>
2026-01-14 20:54:08 +0100

    feat(api): implement POST user endpoint test

--- tests/api/reqres.users.spec.ts
+++ tests/api/reqres.users.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect, request } from "@playwright/test";
 import { logWithStep } from "../helpers/logger.js";
 import constants from "../../data/constants.json";
+import TestData from "../../data/test-data.js";
 
 test.describe("REST API Tests", () => {
     let envConfig = process.env.API_URL ?? "https://reqres.in/api"
@@ -23,5 +24,29 @@ test.describe("REST API Tests", () => {
         // Get list of users
         const userData = await res.json();
         logWithStep("info", `List of users: ${JSON.stringify(userData)}`);
+    });
+
+    // POST Method
+    test("Should create a user", async ({ request }) => {
+        // Make a GET call
+        await logWithStep("info", `Making a POST call using ${envConfig}`);
+        const payload = TestData.apiUserReqresCreation()[0];
+
+        const res = await request.post(`${envConfig}${constants.REQ_RES_ENDPOINTS.POST_USER}`, {
+            headers: {
+                "x-api-key": process.env.REQ_RES_API_KEY,
+                "Content-Type": "application/json",
+            },
+            data: payload,
+        });
+
+        // Assert the status code
+        console.log("Response Status:", res.status());
+        expect(res.status()).toBe(201);
+        await logWithStep("info", `The POST call is succesfull with ${res.status()}`);
+
+        // Get list of users
+        const restData = await res.json();
+        logWithStep("info", `Response data from post call: ${JSON.stringify(restData)}`);
     });
 });
