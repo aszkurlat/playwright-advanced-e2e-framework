be64db1064d1660676949b7830d1be3c4c3d796e
Agnieszka Szkurłat <szkurlatagnieszka@gmail.com>
2026-01-15 19:47:01 +0100

    chore: add file read helper

--- tests/helpers/file-helper.ts
+++ tests/helpers/file-helper.ts
@@ -1,9 +1,14 @@
-//zakomentowałam, temat do którego można wrócić później
-
 import fs from "fs";
 import { logWithStep } from "./logger.js";
 
-
+function readFile(filePath: string): any {
+    if (!fs.existsSync(filePath)) {
+        throw new Error(`No file exists with given name:${filePath}`);
+    }
+    logWithStep("info", `Reading file: ${filePath}...`);
+    let data = fs.readFileSync(filePath, "utf8");
+    return data;
+}
 function writeFile(filePath: string, data: string) {
     try {
         fs.writeFileSync(filePath, data);
@@ -13,4 +18,4 @@ function writeFile(filePath: string, data: string) {
     }
 }
 
-export default { writeFile };
+export default { readFile, writeFile };
