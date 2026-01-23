3d9f245bf39110db536728de256a7249828bfb6b
Agnieszka Szkurłat <szkurlatagnieszka@gmail.com>
2026-01-23 23:29:55 +0100

    test: add EnvConfig type for Playwright

--- tests/helpers/config.fixtures.ts
+++ tests/helpers/config.fixtures.ts
@@ -1,5 +1,16 @@
 import { test as base } from "@playwright/test";
 
+/**
+ * Represents the configuration settings for a specific environment.
+ *
+ * @property envName - The name of the environment (e.g., "development", "staging", "production").
+ *   This variable is used to distinguish between different deployment or testing environments,
+ *   allowing the application or tests to adapt their behavior accordingly.
+ * @property appURL - The base URL of the application under test.
+ * @property dbConfig - The database configuration object for the environment.
+ * @property nopCommerceWeb - The URL for the nopCommerce web application.
+ * @property apiURL - The base URL for the API endpoints.
+ */
 export type EnvConfig = {
     envName: string;
     appURL: string;
@@ -8,11 +19,3 @@ export type EnvConfig = {
     apiURL: string;
 };
 
-export const test = base.extend<EnvConfig>({
-    // Define an option and provide a default value.
-    envName: ["test", { option: true }],
-    appURL: ["<provideURL>", { option: true }],
-    dbConfig: [{}, { option: true }],
-    nopCommerceWeb: ["provideURL", { option: true }],
-    apiURL: ["<provideURL>", { option: true }],
-});
